c1181be88dbb39c342403e20ed56a54b02c988f6
Tommaso Ricci <zorgatone@gmail.com>
2018-09-09 18:24:57 +0200

    Example axios get AJAX call to load posts into Blog state

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
+import axios from "axios";
+
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
+
 import "./Blog.css";
 
 class Blog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentWillMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      this.setState({ posts: response.data });
+    });
+  }
+
   render() {
     return (
       <div>
