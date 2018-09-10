9f18ce3965e7f32b29d91cbd9ae649db683338bb
Tommaso Ricci <zorgatone@gmail.com>
2018-09-10 19:13:32 +0200

    delete post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -11,6 +11,8 @@ class FullPost extends Component {
     this.state = {
       loadedPost: null
     };
+
+    this.deletePostHandler = this.deletePostHandler.bind(this);
   }
 
   componentDidUpdate() {
@@ -28,22 +30,39 @@ class FullPost extends Component {
     }
   }
 
+  deletePostHandler() {
+    if (this.props.id && this.state.loadedPost) {
+      axios
+        .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+        .then(response => console.log(response));
+    }
+  }
+
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+
     if (this.props.id) {
       post = <p style={{ textAlign: "center" }}>Loading...!</p>;
     }
+
     if (this.state.loadedPost) {
       post = (
         <div className="FullPost">
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            {(this.props.id &&
+              this.state.loadedPost && (
+                <button onClick={this.deletePostHandler} className="Delete">
+                  Delete
+                </button>
+              )) ||
+              null}
           </div>
         </div>
       );
     }
+
     return post;
   }
 }
