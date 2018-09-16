9686686f5270bc9c85ea1b3ab1cc766d65c84885
Tommaso Ricci <zorgatone@gmail.com>
2018-09-16 11:08:05 +0200

    example route render jsx

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
 import Posts from "./Posts";
 
@@ -20,7 +21,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home</h1>} />
       </div>
     );
   }
